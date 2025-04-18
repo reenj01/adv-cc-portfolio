fa166e4f8183ea7051e9b4a197b8f9426b16b0b7
reenj01 <ireenjeang0726@gmail.com>
2025-04-18 00:18:10 -0400

    add perfume quiz

--- perfume-quiz-code/main.js
+++ perfume-quiz-code/main.js
@@ -49,3 +49,86 @@ function resetPts() {
   localStorage.setItem("middlepts", 0);
   localStorage.setItem("bottompts", 0);
 }
+
+// Result Page
+
+let finalTop = parseInt(localStorage.getItem("toppts"));
+let finalMiddle = parseInt(localStorage.getItem("middlepts"));
+let finalBottom = parseInt(localStorage.getItem("bottompts"));
+
+function finalResult() {
+  let topname = "";
+  let middlename = "";
+  let bottomname = "";
+
+  if(finalTop >= 0 && finalTop <= 8) {
+    document.getElementById("top-note").innerHTML = "Vanilla";
+    topname = "Vanilla";
+  }
+  else if (finalTop >= 9 && finalTop <= 15) {
+    document.getElementById("top-note").innerHTML = "Citrus";
+    topname = "Citrus";
+  }
+  
+  if(finalMiddle >= 0 && finalMiddle <= 8) {
+    document.getElementById("middle-note").innerHTML = "Herbal";
+    middlename = "Herbal";
+  }
+  else if (finalMiddle >= 9 && finalMiddle <= 15) {
+    document.getElementById("middle-note").innerHTML = "Floral";
+    middlename = "Floral";
+  }
+  
+  if(finalBottom >= 0 && finalBottom <= 8) {
+    document.getElementById("bottom-note").innerHTML = "Wood";
+    bottomname = "Wood";
+  }
+  else if (finalBottom >= 9 && finalBottom <= 15) {
+    document.getElementById("bottom-note").innerHTML = "Spice";
+    bottomname = "Spice";
+  }
+
+  if(topname === "Citrus" && middlename === "Floral" && bottomname === "Spice") {
+    document.getElementById('result-img').src = "../imgs/result/1.PNG";
+    document.getElementById('scent-prof').innerHTML = "honey, sweet";
+    document.getElementById('perfume').innerHTML = "Byredo 'Sundazed'";
+    document.getElementById('perfume2').innerHTML = "Armani Prive 'Saffron Royale'";
+  } else if(topname === "Citrus" && middlename === "Floral" && bottomname === "Wood") {
+    document.getElementById('result-img').src = "../imgs/result/2.PNG";
+    document.getElementById('scent-prof').innerHTML = "woody, cozy";
+    document.getElementById('perfume').innerHTML = "Diptyque 'Eau des Sens'";
+    document.getElementById('perfume2').innerHTML = "Le Labo 'Another 13'";
+  } else if(topname === "Citrus" && middlename === "Herbal" && bottomname === "Spice") {
+    document.getElementById('result-img').src = "../imgs/result/3.PNG";
+    document.getElementById('scent-prof').innerHTML = "fresh, vibrant";
+    document.getElementById('perfume').innerHTML = "Jo Malone 'Basil & Neroli'";
+    document.getElementById('perfume2').innerHTML = "Maison Margiela 'Under the Lemon Trees'";
+  } else if(topname === "Citrus" && middlename === "Herbal" && bottomname === "Wood") {
+    document.getElementById('result-img').src = "../imgs/result/4.PNG";
+    document.getElementById('scent-prof').innerHTML = "cool, crisp";
+    document.getElementById('perfume').innerHTML = "Diptyque 'Oyedo'";
+    document.getElementById('perfume2').innerHTML = "Maison Margiela 'Sailing Day'";
+  } else if(topname === "Vanilla" && middlename === "Floral" && bottomname === "Spice") {
+    document.getElementById('result-img').src = "../imgs/result/5.PNG";
+    document.getElementById('scent-prof').innerHTML = "sweet, cozy";
+    document.getElementById('perfume').innerHTML = "Jo Malone 'Vanilla & Anise'";
+    document.getElementById('perfume2').innerHTML = "Estée Lauder 'Sensuous'";
+  } else if(topname === "Vanilla" && middlename === "Floral" && bottomname === "Wood") {
+    document.getElementById('result-img').src = "../imgs/result/6.PNG";
+    document.getElementById('scent-prof').innerHTML = "rich, musky";
+    document.getElementById('perfume').innerHTML = "Byredo 'Oud Immortel'";
+    document.getElementById('perfume2').innerHTML = "Juliette Has a Gun 'Vanilla Vibes'";
+  } else if(topname === "Vanilla" && middlename === "Herbal" && bottomname === "Spice") {
+    document.getElementById('result-img').src = "../imgs/result/7.PNG";
+    document.getElementById('scent-prof').innerHTML = "mint, sweet";
+    document.getElementById('perfume').innerHTML = "Diptyque 'Eau Duelle'";
+    document.getElementById('perfume2').innerHTML = "Lush 'Lord of Misrule'";
+  } else if(topname === "Vanilla" && middlename === "Herbal" && bottomname === "Wood") {
+    document.getElementById('result-img').src = "../imgs/result/8.PNG";
+    document.getElementById('scent-prof').innerHTML = "earthy, woody";
+    document.getElementById('perfume').innerHTML = "Juliette Has a Gun 'Not a Perfume'";
+    document.getElementById('perfume2').innerHTML = "Le Labo 'Santal 33'";
+  }
+}
+
+finalResult();
